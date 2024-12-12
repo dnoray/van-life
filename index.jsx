47fb9c52b45843ac040964b53c123631c5a0d7e8
Don <dnoray@users.noreply.github.com>
2024-12-11 20:54:28 -0500

    added footer

--- index.jsx
+++ index.jsx
@@ -17,6 +17,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
+      <footer>@2024 VanLife.</footer>
     </BrowserRouter>
   )
 }
